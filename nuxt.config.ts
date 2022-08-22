1ddeb5914bd7d37885ce01ce9901de97278cb26d
Steffen Nilsen <steffennilsen@gmail.com>
2022-08-22 12:48:30 +0200

    switched to server render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
+  target: 'server',
   typescript: {
     shim: false,
   },
